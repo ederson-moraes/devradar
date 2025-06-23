5c371bd114af6a79ba702baa5c4ed66fec8eabc2
Ederson Moraes <eder.israel.moraes@gmail.com>
2025-06-23 23:52:30 +0100

    search controller with geo

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,35 +1,13 @@
 const { Router } = require('express')
-const axios = require('axios')
-const Dev = require('./models/Dev')
 
+const DevController = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
 
 const routes = Router()
 
-routes.post('/devs', async (req, res) => {
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store)
 
-    const { github_username, techs } = req.body
+routes.get('/search', SearchController.index)
 
-    if (!github_username || !techs) {
-        return res.status(400).json({ error: 'GitHub username and techs are required' })
-    }
-
-    const response = await axios.get(`https://api.github.com/users/${github_username}`)
-
-    const { name = login, bio, avatar_url } = response.data
-    const techsArray = techs.split(',').map(tech => tech.trim())
-
-    const dev = await Dev.create({
-        name,
-        github_username,
-        bio,
-        avatar_url,
-        techs: techsArray,
-    })
-
-    return res.json(dev)
-
-})
-
-module.exports = routes;
-
-
+module.exports = routes
